04fb590258fb29aae6be38da204996a8230e7464
Voyager2718 <voyager2718@gmail.com>
2017-05-14 00:57:03 +0800

    Added HP bar

--- WarOfTank/War Of Tank/Assets/Scripts/UI/UIDirectionControl.cs
+++ WarOfTank/War Of Tank/Assets/Scripts/UI/UIDirectionControl.cs
@@ -2,10 +2,9 @@ using UnityEngine;
 
 public class UIDirectionControl : MonoBehaviour
 {
-    public bool m_UseRelativeRotation = true;  
+    public bool m_UseRelativeRotation = true;
 
-
-    private Quaternion m_RelativeRotation;     
+    private Quaternion m_RelativeRotation;
 
 
     private void Start()
